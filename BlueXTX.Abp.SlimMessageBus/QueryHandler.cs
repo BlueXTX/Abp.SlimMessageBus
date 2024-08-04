84e5208063869f4718dca3218f118e3895497013
bluextx <bluextchannel@gmail.com>
2024-08-05 01:40:51 +0300

    Added abstract classes for handlers

---
+++ BlueXTX.Abp.SlimMessageBus/QueryHandler.cs
@@ -0,0 +1,11 @@
+﻿using FluentResults;
+using SlimMessageBus;
+using Volo.Abp.DependencyInjection;
+
+namespace BlueXTX.Abp.SlimMessageBus;
+
+public abstract class QueryHandler<TQuery, TAnswer> : IRequestHandler<TQuery, IResult<TAnswer>>, ITransientDependency
+{
+    Task<IResult<TAnswer>> IRequestHandler<TQuery, IResult<TAnswer>>.OnHandle(TQuery query) => Handle(query);
+    protected abstract Task<IResult<TAnswer>> Handle(TQuery query);
+}
